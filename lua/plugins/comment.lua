a25a48a8cf6e1de70a74b877f43cc4bdb0996d0d
masterj <masterj122517@163.com>
2023-12-24 14:25:10 +0800

    change comment settings

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,41 @@
+local G = require('G')
+local M = {}
 return {
     "yaocccc/vim-comment",
     config = function()
-        vim.cmd([[
-        nmap <silent> ?? :NToggleComment<CR>
-        xmap <silent> /  :<c-u>VToggleComment<CR>
-        smap <silent> /  <c-g>:<c-u>VToggleComment<CR>
-        xmap <silent> ?  :<c-u>CToggleComment<CR>
-        smap <silent> ?  <c-g>:<c-u>CToggleComment<CR>
-        ]]) 
+        -- vim-comment
+        G.g.vim_line_comments = {
+            vim = '"',
+            vimrc = '"',
+            js = '//',
+            ts = '//',
+            java = '//',
+            class = '//',
+            c = '//',
+            h = '//',
+            go = '//',
+            lua = '--',
+            proto = '//',
+            ['go.mod'] = '//',
+            md = '[^_^]:',
+            vue = '//',
+            sql = '--',
+            sol = '//',
+        }
+        G.g.vim_chunk_comments = {
+            js = {'/**', ' *', ' */'},
+            ts = {'/**', ' *', ' */'},
+            sh = {':<<!', '', '!'},
+            proto = {'/**', ' *', ' */'},
+            md = {'[^_^]:', '    ', ''},
+            vue = {'/**', ' *', ' */'},
+            sol = {'/**', ' *', ' */'},
+        }
+        G.map({
+            { 'n', '??', ':NToggleComment<cr>', {silent = true, noremap = true}},
+            { 'v', '/', ':<c-u>VToggleComment<cr>', {silent = true, noremap = true}},
+            { 'v', '?', ':<c-u>CToggleComment<cr>', {silent = true, noremap = true}},
+        })
     end
 }
 
