050cbdde7e6c2764ee63f0d91335fa1ad87c3959
masterj <masterj122517@163.com>
2023-12-30 01:06:26 +0800

    vimtex

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,25 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 
--- My Config 
- --__  __    _    ____ _____ _____ ____     _ 
+-- My Config
+--__  __    _    ____ _____ _____ ____     _
 --|  \/  |  / \  / ___|_   _| ____|  _ \   | |
 --| |\/| | / _ \ \___ \ | | |  _| | |_) |  | |
 --| |  | |/ ___ \ ___) || | | |___|  _ < |_| |
---|_|  |_/_/   \_\____/ |_| |_____|_| \_\___/ 
-                                            
+--|_|  |_/_/   \_\____/ |_| |_____|_| \_\___/
+
 
 
 require("lazy").setup({
@@ -29,7 +29,7 @@ require("lazy").setup({
     require("plugins.scrollbar"),
     require("plugins.git"),
     require("plugins.statusline"),
-    require("plugins.comment"),    
+    require("plugins.comment"),
     require("plugins.indent"),
     require("plugins.cellular-automation"),
     require("plugins.treesitter"),
@@ -41,6 +41,6 @@ require("lazy").setup({
     require("plugins.fcitx"),
     require("plugins.harpoon"),
     require("plugins.undo"),
+    require("plugins.tex"),
+
 })
-
-
