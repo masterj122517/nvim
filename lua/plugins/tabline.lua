1164998cf3443d7db5b2e260d6d8cddbcbf079d1
masterj <masterj122517@163.com>
2024-05-28 13:57:28 +0800

    tbaline and winbar

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,7 +1,7 @@
 return {
     'akinsho/bufferline.nvim',
-    version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
+    branch = "main",
     opts = {
         options = {
             mode = "tabs",
