21e2f28cbe339e94f7ea74967c0f94f6e934e5f3
masterj <masterj122517@163.com>
2024-06-23 01:50:36 +0800

    compile_function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -250,3 +250,40 @@ autocmd Filetype markdown inoremap <buffer> ,l --------<Enter>
 autocmd Filetype markdown inoremap <buffer> ,h <font color=><++></font><Space><++><Esc>F=a
 ]])
 
+
+local split = function()
+    vim.cmd("set splitbelow")
+    vim.cmd("sp")
+    vim.cmd("res -5")
+end
+local compileRun = function()
+    vim.cmd("w")
+    local ft = vim.bo.filetype
+    if ft == "cpp" then
+        split()
+        vim.cmd("term g++ % -o %< && ./%< && rm %<")
+    elseif ft == "markdown" then
+        vim.cmd(":MarkdownPreviewToggle")
+    elseif ft == 'c' then
+        split()
+        vim.cmd("term gcc % -o %< && ./%< && rm %<")
+    elseif ft == 'javascript' then
+        split()
+        vim.cmd("term node %")
+    elseif ft == 'lua' then
+        split()
+        vim.cmd("term luajit %")
+    elseif ft == 'tex' then
+        vim.cmd(":VimtexCompile")
+    elseif ft == 'go' then
+        split()
+        vim.cmd("term go run %")
+    elseif ft == 'python' then
+        split()
+        vim.cmd("term python %")
+    end
+end
+
+vim.keymap.set('n', 'com', compileRun, { silent = true })
+
+
