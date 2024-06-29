337735e443be20b8224b19ccc30069d9db2fb69a
masterj <masterj122517@163.com>
2024-06-29 13:12:25 +0800

    make custom confiug

---
+++ lua/custom/compile.lua
@@ -0,0 +1,34 @@
+local split = function()
+  vim.cmd("set splitbelow")
+  vim.cmd("sp")
+  vim.cmd("res -5")
+end
+local compileRun = function()
+  vim.cmd("w")
+  local ft = vim.bo.filetype
+  if ft == "cpp" then
+    split()
+    vim.cmd("term g++ % -o %< && ./%< && rm %<")
+  elseif ft == "markdown" then
+    vim.cmd(":MarkdownPreviewToggle")
+  elseif ft == "c" then
+    split()
+    vim.cmd("term gcc % -o %< && ./%< && rm %<")
+  elseif ft == "javascript" then
+    split()
+    vim.cmd("term node %")
+  elseif ft == "lua" then
+    split()
+    vim.cmd("term luajit %")
+  elseif ft == "tex" then
+    vim.cmd(":VimtexCompile")
+  elseif ft == "go" then
+    split()
+    vim.cmd("term go run %")
+  elseif ft == "python" then
+    split()
+    vim.cmd("term python %")
+  end
+end
+
+vim.keymap.set("n", "com", compileRun, { silent = true })
