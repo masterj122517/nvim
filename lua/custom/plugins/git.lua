165754962aa21b83d4cf48c68657be0c5209b264
masterj <masterj122517@163.com>
2025-05-24 19:56:29 +0800

    gitsigns update

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,17 +1,13 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = '┃' }, -- 或者 '▎'、'│'、'▍'
+        change = { text = '▎' }, -- 或者 '┃'、'│'、'▍'
+        delete = { text = '󰍴' }, -- 或者 ''、'🗑'、'━'
+        topdelete = { text = '‾' }, -- 或者 ''
+        changedelete = { text = '▍' }, -- 或者 '▌'
       },
     },
   },
