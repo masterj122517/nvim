e9f846c21d7e033b0769e064d2092e678405e768
masterj <masterj122517@163.com>
2025-05-24 19:16:51 +0800

    thanks to folke

--- lua/custom/plugins/snippets.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'L3MON4D3/LuaSnip',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  config = function()
-    require('luasnip.loaders.from_vscode').lazy_load()
-    require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/lua/hacks/snippets/' }
-  end,
-}
