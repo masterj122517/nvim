102b7c0ec358cacf40bf508a115611caa4d622d8
masterj <masterj122517@163.com>
2025-05-26 14:17:39 +0800

    update

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -52,9 +52,8 @@ return { -- Useful plugin to show you pending keybinds.
     },
   },
   {
-
     'folke/todo-comments.nvim',
-    event = 'VimEnter',
+    event = 'VeryLazy',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
@@ -128,5 +127,39 @@ return { -- Useful plugin to show you pending keybinds.
     event = 'InsertEnter',
     ft = { 'html', 'javascript', 'typescript', 'tsx', 'vue' },
     opts = {},
+  },
+  {
+    'sustech-data/wildfire.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('wildfire').setup()
+    end,
+  },
+  {
+    'NvChad/nvim-colorizer.lua',
+    opts = {
+      filetypes = { '*' },
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = true, -- "Name" codes like Blue or blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = false, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = 'virtualtext', -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = true,
+        sass = { enable = false },
+        virtualtext = '■',
+      },
+      -- all the sub-options of filetypes apply to buftypes
+      buftypes = {},
+    },
   },
 }
