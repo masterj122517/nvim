61428d81366c3a070f21b1c1ec61fa7c4580e1f3
masterj <masterj122517@163.com>
2026-03-14 14:17:21 +0800

    some update

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -1,20 +1,22 @@
 return {
-  -- {
-  --   'eatgrass/maven.nvim',
-  --   event = 'VeryLazy',
-  --   cmd = { 'Maven', 'MavenExec' },
-  --   dependencies = 'nvim-lua/plenary.nvim',
-  --   config = function()
-  --     require('maven').setup {
-  --       executable = './mvnw',
-  --     }
-  --   end,
-  -- },
-  -- -- {
-  --   'nvim-java/nvim-java',
-  --   config = function()
-  --     require('java').setup()
-  --     vim.lsp.enable 'jdtls'
-  --   end,
-  -- },
+  {
+    'eatgrass/maven.nvim',
+    event = 'VeryLazy',
+    ft = 'java',
+    cmd = { 'Maven', 'MavenExec' },
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('maven').setup {
+        executable = './mvnw',
+      }
+    end,
+  },
+  {
+    'nvim-java/nvim-java',
+    ft = 'java',
+    config = function()
+      require('java').setup()
+      vim.lsp.enable 'jdtls'
+    end,
+  },
 }
