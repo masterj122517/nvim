dd411af0823b9e84fa3d8434b66931161f397342
masterj <masterj122517@163.com>
2026-06-02 18:20:38 +0800

    refactor: remove some plugins

--- lua/custom/plugins/indent_line.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    enabled = false, -- 统一用 snacks.indent，减少重复渲染
-    opts = {},
-  },
-}
--- lua/custom/plugins/typescript.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'pmizio/typescript-tools.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  opts = {},
-  ft = { 'typescript', 'typescriptreact', 'vue' },
-}
