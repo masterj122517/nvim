19f2089295d9891324d77e57363e44488c39f4e5
masterj <masterj122517@163.com>
2026-06-08 12:20:43 +0800

    feat: add flash.nvim

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -266,16 +266,50 @@ return { -- Useful plugin to show you pending keybinds.
   },
   {
     'folke/flash.nvim',
-    event = 'BufReadPost',
-    opts = {
-      modes = {
-        char = {
-          enabled = true,
-          keys = { 'f', 'F', 't', 'T', ';' },
-          jump_labels = true,
-        },
-        search = { enabled = false },
-        treesitter = { enabled = false },
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-x>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
       },
     },
   },
