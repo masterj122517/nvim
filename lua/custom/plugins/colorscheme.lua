dd4c9ddcddf9c73f35092b39c0f8a409e47d3746
masterj <masterj122517@163.com>
2026-06-09 19:03:42 +0800

    fix: some autocmd fix and change colorshem

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,23 +1,3 @@
-local function start_colorscheme_manager()
-  local themes = { 'everforest', 'tokyonight', 'rose-pine-main', 'gruber-darker', 'colorbuddy', 'gruvbuddy' }
-  math.randomseed(os.time())
-  local current_index = math.random(#themes)
-  local uv = vim.loop or vim.uv
-  local timer = uv.new_timer()
-  local interval = 1 * 60 * 60 * 1000
-
-  timer:start(
-    0,
-    interval,
-    vim.schedule_wrap(function()
-      local theme = themes[current_index]
-      -- 注意：此处拼写已修正为 colorscheme
-      pcall(vim.cmd.colorscheme, theme)
-      current_index = current_index % #themes + 1
-    end)
-  )
-end
-
 return {
   {
     'neanias/everforest-nvim',
@@ -107,7 +87,87 @@ return {
     config = function()
       -- 庄园的警钟开始鸣响，Sir
       -- start_colorscheme_manager()
-      vim.cmd 'colorscheme rose-pine-main'
+      --vim.cmd 'colorscheme rose-pine-main'
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = true, -- disables setting the background color.
+        float = {
+          transparent = true, -- enable transparent floating windows
+          solid = false, -- use solid styling for floating windows, see |winborder|
+        },
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+          virtual_text = {
+            errors = { 'italic' },
+            hints = { 'italic' },
+            warnings = { 'italic' },
+            information = { 'italic' },
+            ok = { 'italic' },
+          },
+          underlines = {
+            errors = { 'underline' },
+            hints = { 'underline' },
+            warnings = { 'underline' },
+            information = { 'underline' },
+            ok = { 'underline' },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        auto_integrations = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
