f6d8c510a8dd269ed66acb835c3d57de4457bb47
masterj <masterj122517@163.com>
2026-06-14 22:47:18 +0800

    some update

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -66,10 +66,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
-    -- local builtin = require 'telescope.builtin'
+    local builtin = require 'telescope.builtin'
     -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = '[S]earch [F]iles' })
     -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
